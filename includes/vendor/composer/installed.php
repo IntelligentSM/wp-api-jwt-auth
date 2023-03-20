fdd9234b3624ef8207ffe5e033fc76799d4b58c9
Kenny Jackson <dsgamecube@gmail.com>
2023-03-20 10:16:42 -0400

    upgrade dependencies

--- includes/vendor/composer/installed.php
+++ includes/vendor/composer/installed.php
@@ -1,53 +1,83 @@
-<?php return array(
-    'root' => array(
-        'name' => 'tmeister/wp-api-jwt-auth',
-        'pretty_version' => 'dev-develop',
-        'version' => 'dev-develop',
-        'reference' => 'd5181b702e0481f26073987932f99dc3ef4a6c08',
-        'type' => 'wordpress-plugin',
-        'install_path' => __DIR__ . '/../../../',
-        'aliases' => array(),
-        'dev' => false,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        'composer/installers' => array(
-            'pretty_version' => 'v1.12.0',
-            'version' => '1.12.0.0',
-            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
-            'type' => 'composer-plugin',
-            'install_path' => __DIR__ . '/./installers',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'firebase/php-jwt' => array(
-            'pretty_version' => 'v6.3.0',
-            'version' => '6.3.0.0',
-            'reference' => '018dfc4e1da92ad8a1b90adc4893f476a3b41cb8',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../firebase/php-jwt',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'roundcube/plugin-installer' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => '*',
-            ),
-        ),
-        'shama/baton' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => '*',
-            ),
-        ),
-        'tmeister/wp-api-jwt-auth' => array(
-            'pretty_version' => 'dev-develop',
-            'version' => 'dev-develop',
-            'reference' => 'd5181b702e0481f26073987932f99dc3ef4a6c08',
-            'type' => 'wordpress-plugin',
-            'install_path' => __DIR__ . '/../../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
+    'reference' => '225ca878cb7d7547c3db120588048c21d9052c4c',
+    'name' => 'tmeister/wp-api-jwt-auth',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.12.0',
+      'version' => '1.12.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
     ),
+    'dealerdirect/phpcodesniffer-composer-installer' => 
+    array (
+      'pretty_version' => 'v1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4be43904336affa5c2f70744a348312336afd0da',
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v6.4.0',
+      'version' => '6.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4dd1e007f22a927ac77da5a3fbb067b42d3bc224',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.7.2',
+      'version' => '3.7.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ed8e00df0a83aa96acf703f8c2979ff33341f879',
+    ),
+    'tmeister/wp-api-jwt-auth' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '225ca878cb7d7547c3db120588048c21d9052c4c',
+    ),
+    'wp-coding-standards/wpcs' => 
+    array (
+      'pretty_version' => '2.3.0',
+      'version' => '2.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7da1894633f168fe244afc6de00d141f27517b62',
+    ),
+  ),
 );
